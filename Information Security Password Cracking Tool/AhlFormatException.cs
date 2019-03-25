d38577c953c282b249c9daa650660c97b82093b9
AAhlbrandt <alec@ahlbrandt.net>
2019-03-24 21:35:56 -0500

    add my own exception class for regex check

---
+++ Information Security Password Cracking Tool/AhlFormatException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Information_Security_Password_Cracking_Tool {
+    class AhlFormatException : Exception {
+        private static string baseMessage = "Password must be of format: length 2-5, ";
+        public AhlFormatException(string message) : base(baseMessage + message) {
+        }
+    }
+}
